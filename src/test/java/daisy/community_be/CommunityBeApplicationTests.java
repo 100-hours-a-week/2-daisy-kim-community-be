1f0e81ceeb99d3ad94f8ec96b0b84bff9cc41511
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-03-19 17:28:35 +0900

    Chore : 프로젝트 초기 설정

---
+++ src/test/java/daisy/community_be/CommunityBeApplicationTests.java
@@ -0,0 +1,13 @@
+package daisy.community_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
