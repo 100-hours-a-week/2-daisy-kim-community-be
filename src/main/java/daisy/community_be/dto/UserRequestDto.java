56280eec8fcab80d305460d12254b672dcfe3da3
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-03-26 15:30:40 +0900

    Feat : 회원가입 시 profileImageUrl을 선택값으로 변경

--- src/main/java/daisy/community_be/dto/UserRequestDto.java
+++ src/main/java/daisy/community_be/dto/UserRequestDto.java
@@ -1,11 +1,15 @@
 package daisy.community_be.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserRequestDto {
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
+    @NotBlank
     private String nickname;
     private String profileImageUrl;
 }
